b487daabf60c4d7ed9a69b1f6a88592966bea8c1
walaayousef98 <96727357+walaayousef98@users.noreply.github.com>
2021-12-29 14:44:23 +0200

    implement shared module #3

---
+++ ui/src/shared/shared-module/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MaterialComponentsModule} from 'src/shared/material-components-module/material-components.module'
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialComponentsModule
+  ]
+})
+export class SharedModule { }
