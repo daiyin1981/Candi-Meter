67fdcfc9c39cce5a71bb2e30879d4c774e22e3c9
ZinabHashem <zadel@informatique-eg.com>
2021-12-30 11:25:01 +0200

    update login page #10

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/auth/login/login.component';
 
 const routes: Routes = [
   { path: 'auth',
-    loadChildren: () => import('./auth/auth/auth/auth.module')
+    loadChildren: () => import('./auth/auth/auth.module')
    .then(auth => auth.AuthModule)},
 ];
 
