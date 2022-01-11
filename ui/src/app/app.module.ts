0539070079098294ac1a56c1fd19f04574f8922c
ZinabHashem <zadel@informatique-eg.com>
2022-01-11 13:56:17 +0200

    create layout #17

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
+    CoreModule,
     BrowserAnimationsModule
   ],
   providers: [],
